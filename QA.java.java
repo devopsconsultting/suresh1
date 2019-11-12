02d47fd792102c4ca98a4a4e65e64b676a2d782c
sureshsreddy16 <57609555+sureshsreddy16@users.noreply.github.com>
2019-11-12 08:46:40 +0530

    Hi qa is going to commit

--- QA.java.java
+++ QA.java.java
@@ -2,9 +2,8 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class QA {
   public static void main(String[] args) {
-    System.out.println("Hello, besant");
-System.out.println("flipkart, besant");
+    System.out.println("This is QA ");
   }
 }
